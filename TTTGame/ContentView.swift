d91dd65aae266ae113e35dae238a1bcc8cae44af
devhaute <devhaute@gmail.com>
2023-11-20 15:56:34 +0900

    초기 환경 세팅

---
+++ TTTGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TTTGame
+//
+//  Created by Tom Lee on 11/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TTTGame/TTTGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTTGameApp.swift
+//  TTTGame
+//
+//  Created by Tom Lee on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct TTTGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
