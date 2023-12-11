b9af1065bd3b7bae80fd2ca698966fd261ede56d
devhaute <devhaute@gmail.com>
2023-12-11 14:45:22 +0900

    온라인 게임을 위한 Firebase 세팅 및 Game 모델 구성

---
+++ TTTGame/Models/Game.swift
@@ -0,0 +1,25 @@
+//
+//  Game.swift
+//  TTTGame
+//
+//  Created by kai on 12/9/23.
+//
+
+import Foundation
+
+struct Game: Codable, Identifiable {
+    let id: String
+    
+    var player1ID: String
+    var player1Name: String
+    var player1Score: String
+    
+    var player2ID: String
+    var player2Name: String
+    var player2Score: String
+    
+    var activePlayerID: String
+    var winnigPlayerID: String
+    
+    var moves: [GameMove?]
+}
